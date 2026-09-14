076d9ff0e25c6528b65294d898719c61b45273ca
pax-ad <ad007scrutinize@gmail.com>
2026-09-14 23:27:20 +0530

    Basic functionality done

--- backend/model/HoldingsModel.js
+++ backend/model/HoldingsModel.js
@@ -1,6 +1,6 @@
 const { model } = require("mongoose");
 
-const { HoldingsSchema } = require("../schemas/HoldingsSchema");
+const { HoldingsSchema } = require('../schemas/HoldingsSchema');
 
 const HoldingsModel = new model("holding", HoldingsSchema);
 
